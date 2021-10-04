4eb518173614a4dfdabfd0468e6e098b2a638abb
IneseKo <in.kocina@gmail.com>
2021-10-04 21:34:15 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {{
+        String[] arrayPigs = {"Nifnif", "Nafnaf", "Nufnuf"};
+	for (String piggyNames : arrayPigs) {
+        System.out.println(piggyNames);}
+
+for (int i=0; i< arrayPigs.length;i++)
+{
+    arrayPigs[i] = "Piggy";
+    System.out.println(arrayPigs[i]);
+}
+}
+    }}
