21014e1505ad83b8073e388c87ceb931345ee5dc
IneseKo <in.kocina@gmail.com>
2021-10-08 23:50:41 +0300

    Assignment 2 finished.

--- src/com/company/Assignment2.java
+++ src/com/company/Assignment2.java
@@ -4,17 +4,17 @@ public class Assignment2 {
     public static void main(String[] args) {
         int[] arrOne = {10, 4, -4, 7, 0, 9, 1, 3, 7, -5};
         negativeElements(arrOne);
-        System.out.println();
         oddElements(arrOne);
-        System.out.println();
         countOddElements(arrOne);
-        System.out.println();
         sameValue(arrOne);
-
+        eachSecondElement(arrOne);
+        averageValuePrint(arrOne);
+        smallerThanAverage(arrOne);
     }
 
     public static void negativeElements(int[] arrOne) {
         int i = 0;
+        System.out.println("These are the negative elements of the Array:");
         for (int value : arrOne) {
             if (value < 0) {
                 System.out.println(value);
@@ -25,6 +25,7 @@ public class Assignment2 {
 
     public static void oddElements(int[] arrOne) {
         int i = 0;
+        System.out.println("\nThese are the odd elements of the Array:");
         for (int value : arrOne) {
             if (value % 2 != 1) {
                 System.out.println(value);
@@ -35,24 +36,56 @@ public class Assignment2 {
     }
 
     public static void countOddElements(int[] arrOne) {
-             int sum = 0;
+        int count = 0;
         for (int value : arrOne) {
             if (value % 2 != 1) {
-                               sum = sum+1;
-                System.out.println(sum);      }
+                count++;
+            }
         }
+        System.out.println("\nThere are " + count + " odd Elements in this Array.");
+    }
+
+    public static void sameValue(int[] arrOne) {
+        for (int i = 0; i < arrOne.length; i++) {
+            for (int j = i + 1; j < arrOne.length; j++) {
+                if (arrOne[i] == arrOne[j]) {
+                    System.out.println("\nElement No" + i + " equals Element No" + j + ". Their value is " + arrOne[i] + ".");
+
+                }
+            }
+        }
+    }
+
+    public static void eachSecondElement(int[] arrOne) {
+        System.out.println("\nEvery other Element in this Array is following:");
+        for (int i = 0; i < arrOne.length; i++) {
+            if (i % 2 == 1) {
+                System.out.println(arrOne[i]);
+                i++;
+            }
+        }
+    }
+
+    public static double averageValue(int[] arrOne) {
+        double sum = 0;
+        for (int i = 0; i < arrOne.length; i++) {
+            sum = sum + arrOne[i];
+        }
+        double averageValue = sum / arrOne.length;
+        return averageValue;
+    }
+
+    public static void averageValuePrint(int[] arrOne) {
+        System.out.println("\nThe average value of the array is " + averageValue(arrOne));
+    }
+
+    public static void smallerThanAverage(int[] arrOne) {
+        int count = 0;
+        for (int i = 0; i < arrOne.length; i++) {
+            if (averageValue(arrOne) > arrOne[i]) {
+                count++;
+            }
+        }
+        System.out.println("\nThere are " + count + " elements in the Array that are smaller than the average value" + " (" + averageValue(arrOne) + ").");
+    }
 }
-
-public static void sameValue(int[] arrOne) {
-        int i = 0;
-    for (int value : arrOne) {
-        if (value == arrOne[i]) {
-            System.out.println(value + "=" + i);
-            i++;
-            continue;
-        }
-        System.out.println("There are no elements with the same value");
-    }
-
-        }
-    }
