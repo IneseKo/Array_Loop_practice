97c7e8a9a7b52979d70d1b425bd0a7a9ac3104c4
IneseKo <in.kocina@gmail.com>
2021-10-10 01:38:51 +0300

    Assignment 3 partly done but updated array filling part.

--- src/com/company/Assignment3.java
+++ src/com/company/Assignment3.java
@@ -4,18 +4,33 @@ import java.util.Scanner;
 
 public class Assignment3 {
     public static void main(String[] args) {
-        byte[] arrOne = new byte[10];
-        int i = 0;
-        int sequenceNo = 1;
-        for (byte grade : arrOne) {
-            Scanner inputGrade = new Scanner(System.in);
-            System.out.println("Please enter grade No" + sequenceNo++);
+        byte[] arrOne = new byte[3];
+        arrFill(byte[] arrOne);
+        arrPrint(byte[] arrOne);
+    }
+        public static void arrFill (byte[] arrOne){
+            int i = 0;
+            int sequenceNo = 1;
 
-            grade = inputGrade.nextByte();
-            if (grade >= 0 && grade <= 10) {
-                arrOne[i] = inputGrade.nextByte();
-                i++;
+            while (sequenceNo <= arrOne.length) {
+                Scanner inputGrade = new Scanner(System.in);
+                System.out.println("Please enter grade No" + sequenceNo);
+                byte grade = inputGrade.nextByte();
+
+                if (grade > 0 && grade <= 10) {
+                    arrOne[i++] = grade;
+                    sequenceNo++;
+                } else {
+                    System.out.println("Wrong data");
+                }
             }
         }
-    }
+public static void arrPrint(byte[] arrOne){
+        System.out.println("The grades You entered are as follows:");
+        for (int k = 0; k < arrOne.length; k++)
+            System.out.println(arrOne[k]);
+
+
 }
+
+
